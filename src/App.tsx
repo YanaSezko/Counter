9c5ca278465a21b85cd77c32e05c84ed0b28dee8
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-11-02 22:35:50 +0200

    Counter2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState,ChangeEvent} from 'react';
 import Button1 from "./components/Button1";
 import "./App.css"
 
-
 function App() {
-    let [value, setValue] = useState(0);
+    let[value, setValue] = useState(0);
+    let[maxValue,setMaxValue]=useState(0)
+    let[startValue,setStartValue]=useState(0)
 
     function addValue() {
        setValue(++value)
@@ -12,16 +13,49 @@ function App() {
     function changeDisRes() {
         setValue(0)
     }
+
+    let changeValueMax = (e: ChangeEvent<HTMLInputElement>)=> {
+        let maxValue=e.currentTarget.value
+        setMaxValue(Number(maxValue))
+        localStorage.setItem("maxValue",maxValue)
+   
+    }
+
+
+    let changeValueStart=(e: ChangeEvent<HTMLInputElement>)=>{
+        let startValue=e.currentTarget.value
+        setStartValue(Number(startValue))
+        localStorage.setItem("startValue",startValue)
+   } 
+
+   debugger
+   const setInputValue=(startValue:any,maxValue:any)=>{
+  value=startValue
+  setValue(value)
+
+  if(value>=maxValue){
+      alert("error")
+  }
+
+   }
+
     return (
         <div className={"App"}>
-            <span className={(value===5?"red":"")}>{value}</span>
+            <span className={(value===maxValue? "red" : "" )}>{value}</span>
             <div className={"wrapper"}>
-                <Button1 id={1} value={value} addValue={addValue} />
+                <Button1 id={1} value={value} addValue={addValue} maxValue={maxValue}/>
                 <Button1 id={2} value={value} changeDisRes={changeDisRes}/>
+                <Button1 id={3} value={value} onClick={()=>{setInputValue(startValue,maxValue)}}/>
             </div>
+            start<input type="number" min={startValue} max={maxValue} step={1} onChange={changeValueStart} /><br/>
+            max<input type="number" min={startValue} max={maxValue}  step={1} onChange={changeValueMax}  />
+           
+            
         </div>
 
     );
 }
 
+
 export default App;
+
--- src/components/Button1.tsx
+++ src/components/Button1.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 type ButtonPropsType = {
     id: number
-    value: number
+    value:any
     addValue?: any
     changeDisRes?: any
+    onClick?:any
+    maxValue?:any
+
 }
 
 const Button1 = (props: ButtonPropsType) => {
 
     return (
         <div>
-            {props.id === 1 && <button onClick={props.addValue} disabled={props.value >= 5} >inc</button>}
+            {props.id === 1 && <button onClick={props.addValue} disabled={props.value >= props.maxValue} >inc</button>}
             {props.id === 2 && <button onClick={props.changeDisRes} disabled={props.value<=0}>reset</button>}
+            {props.id === 3 && <button onClick={props.onClick} >set</button>}
         </div>
     );
 }
